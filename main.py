c90d8a2ef1f828137942c9b6bae103f345d387bd
Derrick Biana <djmbiana@gmail.com>
2025-11-16 23:37:49 +0800

    added edit feature

--- main.py
+++ main.py
@@ -47,11 +47,33 @@ def add(description):
         print(f"Status: {user_task['status']}")
         print(f"Created On: {user_task['time_created']}")
 
+# update tasks
 @taskie.command()
+@click.argument("id", type=int)
 @click.argument("description")
 def update(id, description):
-    pass
+    time_updated = datetime.datetime.now().replace(microsecond=0)
 
+    with open("tasks.json", mode="r") as json_record:
+        data = json.load(json_record)
+        for record in data:
+            if record['id'] == id:
+               record['description'] = description 
+               record['time_updated'] = time_updated
+            break
+               
+    try:
+        with open("tasks.json", mode="w") as json_file:
+            json.dump(data, json_file, indent=4, default=str)
+    finally:
+        print(f"Time Update: {time_updated}") 
+
+@taskie.command()
+@click.arguement("id", type=int)
+def delete(id):
+    with open("tasks.json", mode="r") as json_record:
+        data = json.load(json_record)
+    
 
 # view all tasks
 @taskie.command()
