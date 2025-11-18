e1a920b6307853dc23eccc8f5764baf6496adb18
Derrick Biana <djmbiana@gmail.com>
2025-11-18 12:58:32 +0800

    project is now in MVP state. will make README and ship to roadmap.sh later.

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ else:
 def taskie():
     pass
 
+
 # add tasks
 @taskie.command()
 @click.argument("description")
@@ -47,6 +48,7 @@ def add(description):
         print(f"Status: {user_task['status']}")
         print(f"Created On: {user_task['time_created']}")
 
+
 # update tasks
 @taskie.command()
 @click.argument("id", type=int)
@@ -57,36 +59,155 @@ def update(id, description):
     with open("tasks.json", mode="r") as json_record:
         data = json.load(json_record)
         for record in data:
-            if record['id'] == id:
-               record['description'] = description 
-               record['time_updated'] = time_updated
-            break
-               
+            if record["id"] == id:
+                record["description"] = description
+                record["time_updated"] = time_updated
+                break
+
     try:
         with open("tasks.json", mode="w") as json_file:
             json.dump(data, json_file, indent=4, default=str)
     finally:
-        print(f"Time Update: {time_updated}") 
+        print(f"Time Update: {time_updated}")
 
+# delete tasks
 @taskie.command()
 @click.argument("id", type=int)
 def delete(id):
     with open("tasks.json", mode="r") as json_record:
         data = json.load(json_record)
-    
+        
+    found = False
+    for id_del, task in enumerate(data):
+        if task['id'] == id:
+            data.pop(id_del)
+            found = True
+            break
+
+    if not found:
+        print(f"Task {id} not found")
+        return
+
+    with open("tasks.json", mode="w") as json_file:
+            json.dump(data, json_file, indent=4, default=str)
+    print(f"Task {id} has been deleted.")
+
+
+# mark task as done
+@taskie.command()
+@click.argument("id", type=int)
+def mark_as_done(id):
+    time_updated = datetime.datetime.now().replace(microsecond=0)
+
+    with open("tasks.json", mode="r") as json_record:
+        data = json.load(json_record)
+        for record in data:
+            if record["id"] == id:
+                record["status"] = "done"
+                record["time_updated"] = time_updated
+                break
+    try:
+        with open("tasks.json", mode="w") as json_file:
+            json.dump(data, json_file, indent=4, default=str)
+
+    finally:
+        print("Tasks status has been updated!")
+
+# mark task in progress
+@taskie.command()
+@click.argument("id", type=int)
+def mark_in_progress(id):
+    time_updated = datetime.datetime.now().replace(microsecond=0)
+
+    with open("tasks.json", mode="r") as json_record:
+        data = json.load(json_record)
+        for record in data:
+            if record["id"] == id:
+                record["status"] = "in-progress"
+                record["time_updated"] = time_updated
+                break
+    try:
+        with open("tasks.json", mode="w") as json_file:
+            json.dump(data, json_file, indent=4, default=str)
+
+    finally:
+        print("Tasks status has been updated!")
+
 
 # view all tasks
 @taskie.command()
 def view():
-        with open("tasks.json", mode="r") as json_file:
-            data = json.load(json_file)
-        for task in data:
-            print(f"Task ID: {task['id']}")
-            print(f"Task Description: {task['description']}")
-            print(f"Status: {task['status']}")
-            print(f"Created On: {task['time_created']}")
-            print(f"Updated On: {task['time_updated']}")
-            print("=" * 30)
+    with open("tasks.json", mode="r") as json_file:
+        data = json.load(json_file)
+
+    click.echo(f"\nAll current tasks: \n")
+    for task in data:
+        print(f"Task ID: {task['id']}")
+        print(f"Task Description: {task['description']}")
+        print(f"Status: {task['status']}")
+        print(f"Created On: {task['time_created']}")
+        print(f"Updated On: {task['time_updated']}")
+        print("=" * 30)
+
+
+
+# view tasks that are todo
+@taskie.command()
+def view_todo():
+    with open("tasks.json", mode="r") as json_file:
+        data = json.load(json_file)
+    
+    done_tasks = [task for task in data if task.get('status') == 'todo']
+
+    click.echo(f"\nunfinished work:\n")
+    
+    for task in done_tasks:
+        print(f"Task ID: {task['id']}")
+        print(f"Task Description: {task['description']}")
+        print(f"Status: {task['status']}")
+        print(f"Created On: {task['time_created']}")
+        print(f"Updated On: {task['time_updated']}")
+        print("=" * 30)
+
+# view tasks that are in-progress
+@taskie.command()
+def view_inprogress():
+    with open("tasks.json", mode="r") as json_file:
+        data = json.load(json_file)
+    
+    done_tasks = [task for task in data if task.get('status') == 'in-progress']
+
+    click.echo(f"\nunfinished work:\n")
+    
+    for task in done_tasks:
+        print(f"Task ID: {task['id']}")
+        print(f"Task Description: {task['description']}")
+        print(f"Status: {task['status']}")
+        print(f"Created On: {task['time_created']}")
+        print(f"Updated On: {task['time_updated']}")
+        print("=" * 30)
+
+# view tasks that are done
+@taskie.command()
+def view_done():
+    with open("tasks.json", mode="r") as json_file:
+        data = json.load(json_file)
+    
+    done_tasks = [task for task in data if task.get('status') == 'done']
+    
+    if not done_tasks:
+        click.echo("No completed tasks found.")
+        return
+    
+    click.echo(f"\nfinished work:\n")
+    
+    for task in done_tasks:
+        print(f"Task ID: {task['id']}")
+        print(f"Task Description: {task['description']}")
+        print(f"Status: {task['status']}")
+        print(f"Created On: {task['time_created']}")
+        print(f"Updated On: {task['time_updated']}")
+        print("=" * 30)
 
 
 if __name__ == "__main__":
